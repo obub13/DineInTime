55cc36690432690478ada5dc7e9b715d6e9f65e8
ShakedDahari <shaked1299@gmail.com>
2023-06-07 16:20:47 +0300

    update

---
+++ server/routes/usersRoute.js
@@ -0,0 +1,53 @@
+const User = require('../models/users');
+const usersRoute = require('express').Router();
+
+usersRoute.get('/api/users', async (req, res) => {
+    try {
+        let data = await User.FindAllUsers();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+});
+
+usersRoute.get('/:id', async (req, res) => {
+    try {
+        let { id } = req.params;
+        let data = await User.FindById(id);
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+});
+
+usersRoute.get('/email/:email', async (req, res) => {
+    try {
+        let { email } = req.params;
+        let data = await User.FindByEmail(email);
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+});
+
+usersRoute.get('/username/:username', async (req, res) => {
+    try {
+        let { username } = req.params;
+        let data = await User.FindByUserName(username);
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+})
+
+usersRoute.post('/add', async (req, res) => {
+    try {
+        let { email, phone, username, image, password, verify } = req.body;
+        let data = await new User(email, phone, username, image, password, verify).InsertOne();
+        res.status(201).json(data);
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+});
+
+module.exports = usersRoute;
