25c4161899e241dfe02970172a28327abe5273bc
ShakedDahari <shaked1299@gmail.com>
2023-06-07 12:29:21 +0300

    update

--- utils/api_url.js
+++ utils/api_url.js
@@ -5,9 +5,9 @@
 // export const axiosURL = 'http://10.57.1.96:5500';
 
 export const railwayURL = 'https://dineintime-production.up.railway.app';
-
-export const renderURL = 'https://dineintime.onrender.com';
-
+//https://weak-erin-piglet-ring.cyclic.app
+export const renderURL = 'https://weak-erin-piglet-ring.cyclic.app';
+//https://dineintime.onrender.com
 export const DATA_API = 'https://eu-central-1.aws.data.mongodb-api.com/app/data-ghtpu/endpoint/data/v1';
 
 export const DEV_API_URL = 'http://localhost:5500';
