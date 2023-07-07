85760753f8b8d3b27f4b44b65f25adf8b7e9445f
obub13 <117099699+obub13@users.noreply.github.com>
2023-07-07 22:32:00 +0300

    javascript shit

--- client/utils/api_url.js
+++ client/utils/api_url.js
@@ -7,7 +7,7 @@
 export const railwayURL = 'https://dineintime-production.up.railway.app';
 //https://dineintime.onrender.com
 // export const apiUrl = 'https://dineintime.onrender.com';
-export const apiUrl ='https://dineintimefull.onrender.com';
+export const apiUrl = 'https://dineintimefull.onrender.com';
 //https://dineintime.cyclic.app
 export const DATA_API = 'https://eu-central-1.aws.data.mongodb-api.com/app/data-ghtpu/endpoint/data/v1';
 
